27d71b20539686b739ddc09ba18e45cc5af4b05f
James Patton <jamesyp@gmail.com>
2023-04-23 15:28:27 -0700

    feat: adds monsters

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ mod prelude {
     pub use crate::camera::*;
     pub use crate::spawner::*;
 }
+
 use prelude::*;
 
 struct State {
@@ -37,6 +38,12 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+
+        map_builder.rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
