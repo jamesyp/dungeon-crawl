93d4a794700b3dae702e76bc626eff35e58b5449
James Patton <jamesyp@gmail.com>
2023-04-23 18:56:06 -0700

    feat: adds player health and HUD

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,6 +1,7 @@
 mod player_input;
 mod map_render;
 mod entity_render;
+mod hud;
 
 mod collisions;
 mod random_move;
@@ -15,6 +16,7 @@ pub fn build_input_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .build()
 }
 
@@ -26,6 +28,7 @@ pub fn build_player_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
@@ -38,6 +41,7 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
+        .add_system(hud::hud_system())
         .add_system(end_turn::end_turn_system())
         .build()
 }
