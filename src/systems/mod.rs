aa99d5ecf1a883059fa71dc07f6bdbee6070f6b7
James Patton <jamesyp@gmail.com>
2023-04-23 18:22:12 -0700

    chore: movement using intent messages

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -5,6 +5,7 @@ mod entity_render;
 mod collisions;
 mod random_move;
 mod end_turn;
+mod movement;
 
 use crate::prelude::*;
 
@@ -19,6 +20,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
@@ -31,7 +34,7 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
