a5d1e44fb6552b9ac9ff4bcbb59deef75c7302dd
James Patton <jamesyp@gmail.com>
2023-05-28 18:07:46 -0700

    feat: adds amulet of yala to dungeon

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,7 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut ecs, map_builder.amulet_start);
 
         map_builder.rooms
             .iter()
@@ -81,11 +82,14 @@ impl State {
             let mut rng = RandomNumberGenerator::new();
             let map_builder = MapBuilder::new(&mut rng);
             spawn_player(&mut self.ecs, map_builder.player_start);
+            spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+
             map_builder.rooms
                 .iter()
                 .skip(1)
                 .map(|r| r.center())
                 .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+
             self.resources.insert(map_builder.map);
             self.resources.insert(Camera::new(map_builder.player_start));
             self.resources.insert(TurnState::AwaitingInput);
