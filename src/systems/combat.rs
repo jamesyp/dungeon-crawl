2d933dbf50881bf39ff098c307d5686fc61ce7fb
James Patton <jamesyp@gmail.com>
2023-05-28 17:43:55 -0700

    feat: adds game over screen

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attackers = <(Entity, &WantsToAttack)>::query();
@@ -12,13 +13,19 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         .collect();
 
     defenders.iter().for_each(|(message, defender)| {
+        let is_player = ecs
+            .entry_ref(*defender)
+            .unwrap()
+            .get_component::<Player>()
+            .is_ok();
+
         if let Ok(mut health) = ecs
             .entry_mut(*defender)
             .unwrap()
             .get_component_mut::<Health>()
         {
             health.current -= 1;
-            if health.current < 1 {
+            if health.current < 1 && !is_player {
                 commands.remove(*defender);
             }
         }
