13e309426ed4d559740f06a9b3d46b3210db05e6
James Patton <jamesyp@gmail.com>
2023-09-16 14:33:09 -0700

    feat: use randomly determined MapArchitect

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -4,7 +4,7 @@ mod automata;
 mod drunkard;
 
 use crate::prelude::*;
-use empty::EmptyArchitect;
+// use empty::EmptyArchitect;
 use rooms::RoomsArchitect;
 use automata::CellularAutomataArchitect;
 use drunkard::DrunkardsWalkArchitect;
@@ -25,7 +25,12 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = DrunkardsWalkArchitect{};
+        let mut architect : Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect{}),
+            1 => Box::new(RoomsArchitect{}),
+            _ => Box::new(CellularAutomataArchitect{}),
+        };
+
         architect.build(rng)
     }
 
