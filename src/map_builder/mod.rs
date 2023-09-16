6f57e1e096ed9508d778d4ac9206a7303b9ed4e9
James Patton <jamesyp@gmail.com>
2023-09-16 15:51:48 -0700

    chore: adds MapTheme trait and dungeon + forest theme

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -4,6 +4,8 @@ mod automata;
 mod drunkard;
 mod prefab;
 
+mod themes;
+
 use crate::prelude::*;
 // use empty::EmptyArchitect;
 use rooms::RoomsArchitect;
@@ -11,8 +13,14 @@ use automata::CellularAutomataArchitect;
 use drunkard::DrunkardsWalkArchitect;
 use prefab::apply_prefab;
 
+use themes::*;
+
 trait MapArchitect {
     fn build(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
+
+pub trait MapTheme : Sync + Send {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType;
 }
 
 const NUM_ROOMS: usize = 20;
@@ -23,6 +31,7 @@ pub struct MapBuilder {
     pub monster_spawns: Vec<Point>,
     pub player_start: Point,
     pub amulet_start: Point,
+    pub theme: Box<dyn MapTheme>
 }
 
 impl MapBuilder {
@@ -34,6 +43,11 @@ impl MapBuilder {
         };
         let mut mb = architect.build(rng);
         apply_prefab(&mut mb, rng);
+
+        mb.theme = match rng.range(0, 2) {
+            0 => DungeonTheme::new(),
+            _ => ForestTheme::new(),
+        };
 
         mb
     }
