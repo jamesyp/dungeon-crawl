c4b0b7038534406ba7990f745684a65d4b11fad2
James Patton <jamesyp@gmail.com>
2023-09-18 21:38:56 -0700

    feat: adds potion and map entities

--- src/spawner.rs
+++ src/spawner.rs
@@ -15,6 +15,15 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
     );
 }
 
+pub fn spawn_entity(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    let roll = rng.roll_dice(1, 6);
+    match roll {
+        1 => spawn_healing_potion(ecs, pos),
+        2 => spawn_magic_map(ecs, pos),
+        _ => spawn_monster(ecs, rng, pos)
+    };
+}
+
 pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
     let (hp, name, glyph) = match rng.roll_dice(1, 10) {
         1..=8 => goblin(),
@@ -31,6 +40,36 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
             Health { current: hp, max: hp },
             Name(name),
             FieldOfView::new(6)
+        )
+    );
+}
+
+pub fn spawn_healing_potion(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (
+            Item,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437('!')
+            },
+            Name("Healing Potion".to_string()),
+            ProvidesHealing{amount: 6}
+        )
+    );
+}
+
+pub fn spawn_magic_map(ecs: &mut World, pos: Point) {
+    ecs.push(
+(
+            Item,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437('{')
+            },
+            Name("Dungeon Map".to_string()),
+            ProvidesDungeonMap{}
         )
     );
 }
