168f08b94df5280cdd30add7e687dc2259a0d19f
James Patton <jamesyp@gmail.com>
2023-09-18 22:50:11 -0700

    chore: adds use item intent

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -29,17 +29,20 @@ pub fn player_input(
             VirtualKeyCode::Up => Point::new(0, -1),
             VirtualKeyCode::Down => Point::new(0, 1),
             VirtualKeyCode::G => {
-                let mut items = <(Entity, &Item, &Point)>::query();
-                items.iter(ecs)
-                    .filter(|(_entity, _item, &item_pos)| item_pos == player_pos)
-                    .for_each(|(entity, _item, _item_pos)| {
-                        commands.remove_component::<Point>(*entity);
-                        commands.add_component(*entity, Carried(player));
-                    });
+                get_item(player, player_pos, ecs, commands);
 
                 did_something = true;
                 Point::zero()
             },
+            VirtualKeyCode::Key1 => use_item(0, ecs, commands),
+            VirtualKeyCode::Key2 => use_item(1, ecs, commands),
+            VirtualKeyCode::Key3 => use_item(2, ecs, commands),
+            VirtualKeyCode::Key4 => use_item(3, ecs, commands),
+            VirtualKeyCode::Key5 => use_item(4, ecs, commands),
+            VirtualKeyCode::Key6 => use_item(5, ecs, commands),
+            VirtualKeyCode::Key7 => use_item(6, ecs, commands),
+            VirtualKeyCode::Key8 => use_item(7, ecs, commands),
+            VirtualKeyCode::Key9 => use_item(8, ecs, commands),
             _ => Point::zero(),
         };
         let destination  = player_pos + delta;
@@ -79,4 +82,39 @@ pub fn player_input(
         }
         *turn_state = TurnState::PlayerTurn;
     }
+}
+
+fn get_item(player: Entity, pos: Point, ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut items = <(Entity, &Item, &Point)>::query();
+    items.iter(ecs)
+        .filter(|(_entity, _item, &item_pos)| item_pos == pos)
+        .for_each(|(entity, _item, _item_pos)| {
+            commands.remove_component::<Point>(*entity);
+            commands.add_component(*entity, Carried(player));
+        });
+}
+
+fn use_item(n: usize, ecs: &mut SubWorld, commands: &mut CommandBuffer) -> Point {
+    let player = <(Entity, &Player)>::query()
+        .iter(ecs)
+        .map(|(entity, _player)| *entity)
+        .next()
+        .unwrap();
+
+    let item = <(Entity, &Item, &Carried)>::query()
+        .iter(ecs)
+        .filter(|(_, _, carried)| carried.0 == player)
+        .enumerate()
+        .filter(|(item_index, (_, _, _))| *item_index == n)
+        .map(|(_, (item, _, _))| *item)
+        .next();
+
+    if let Some(item) = item {
+        commands.push(((), ActivateItem{
+            used_by: player,
+            item
+        }));
+    }
+
+    Point::zero()
 }
