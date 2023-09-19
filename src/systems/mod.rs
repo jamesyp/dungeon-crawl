a07ecbc8d91ebecef7fefb73b1887cacb568e63d
James Patton <jamesyp@gmail.com>
2023-09-18 23:11:07 -0700

    feat: adds use_items system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -6,6 +6,7 @@ mod tooltips;
 
 mod movement;
 mod combat;
+mod use_items;
 mod random_move;
 mod chasing;
 mod end_turn;
@@ -27,6 +28,7 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(use_items::use_items_system())
         .add_system(combat::combat_system())
         .flush()
         .add_system(movement::movement_system())
