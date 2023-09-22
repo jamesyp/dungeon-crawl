3e1384112717385d0b357f5a22e00f805134171a
James Patton <jamesyp@gmail.com>
2023-09-22 16:49:34 -0700

    feat: stairs advance to next level for real

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ mod prelude {
 }
 
 use prelude::*;
+use std::collections::HashSet;
 
 struct State {
     ecs: World,
@@ -120,7 +121,72 @@ impl State {
     }
 
     fn advance_level(&mut self) {
-        self.reset();
+        self.remove_entities();
+
+        <&mut FieldOfView>::query()
+            .iter_mut(&mut self.ecs)
+            .for_each(|fov| fov.is_dirty = true);
+
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = self.next_level_map(&mut rng);
+
+        map_builder.monster_spawns
+            .iter()
+            .for_each(|pos| spawn_entity(&mut self.ecs, &mut rng, *pos));
+        self.resources.insert(map_builder.map);
+        self.resources.insert(map_builder.theme);
+        self.resources.insert(Camera::new(map_builder.player_start));
+        self.resources.insert(TurnState::AwaitingInput);
+    }
+
+    fn remove_entities(&mut self) {
+        let player_entity = *<Entity>::query()
+        .filter(component::<Player>())
+        .iter(&self.ecs)
+        .next()
+        .unwrap();
+
+        let mut entities_to_keep = HashSet::new();
+        entities_to_keep.insert(player_entity);
+
+        <(Entity, &Carried)>::query()
+            .iter(&self.ecs)
+            .filter(|(_e, carry)| carry.0 == player_entity)
+            .map(|(e, _carry)| *e)
+            .for_each(|e|{ entities_to_keep.insert(e); });
+
+        let mut cb = CommandBuffer::new(&self.ecs);
+        for e in Entity::query().iter(&self.ecs) {
+            if !entities_to_keep.contains(e) {
+                cb.remove(*e);
+            }
+        }
+
+        cb.flush(&mut self.ecs);
+    }
+
+    fn next_level_map(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder {
+        let mut map_builder = MapBuilder::new(rng);
+
+        let mut map_level = 0;
+        <(&mut Player, &mut Point)>::query()
+            .iter_mut(&mut self.ecs)
+            .for_each(|(player, pos)| {
+                player.map_level += 1;
+                map_level = player.map_level;
+                pos.x = map_builder.player_start.x;
+                pos.y = map_builder.player_start.y;
+            }
+        );
+
+        if map_level == 2 {
+            spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start)
+        } else {
+            let exit_idx = map_builder.map.point2d_to_index(map_builder.amulet_start);
+            map_builder.map.tiles[exit_idx] = TileType::Exit;
+        }
+
+        map_builder
     }
 }
 impl GameState for State {
